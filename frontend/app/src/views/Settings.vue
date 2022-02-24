7089a75fa9901133fea48dd490c37415d03b6ecc
Dmitry Verteyko <verteyko1990@gmail.com>
2022-02-25 00:16:02 +0300

    added profile info

--- frontend/app/src/views/Settings.vue
+++ frontend/app/src/views/Settings.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="app-profile">
     <div class="app-profile-form">
-      <div class="app-profile-form app-avatar">
-        <img src="../assets/images/placeholders/avatar.svg" alt="">
+<!--      <div class="avatar">-->
+<!--        <input class="avatar__file" type="file">-->
+<!--        <img class="avatar__image" src="../assets/images/placeholders/avatar.jpg" alt="">-->
+<!--      </div>-->
+      <div class="profile-pic"
+           :class="{'disabled': !isEditMode}"
+      >
+        <label class="-label"
+               for="file">
+          <span class="glyphicon glyphicon-camera"></span>
+          <span>Change Image</span>
+        </label>
+        <input id="file" type="file" onchange="loadFile(event)"/>
+        <img src="../assets/images/placeholders/avatar.jpg"
+             id="output" width="200" />
       </div>
       <div class="app-profile-form app-content">
         <div class="app-edit">
@@ -184,5 +197,104 @@ export default class Settings extends Vue {
     grid-template-columns: 40% 40%;
     justify-content: space-between;
     grid-gap: 1fr;
+  }
+
+  .avatar__file {
+    border-radius: 50%;
+    height: 150px;
+    width: 150px;
+    background: red;
+    display: grid;
+    justify-content: center;
+    position: absolute;
+    z-index: 0;
+  }
+
+  .avatar {
+    border-radius: 50%;
+    height: 150px;
+    width: 150px;
+    display: grid;
+    justify-content: center;
+  }
+
+  .avatar__image {
+    border-radius: 50%;
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+    position: relative;
+    &:after {
+      content: '';
+      position: absolute;
+      width: 150px;
+      height: 150px;
+      background: #e41111;
+      top: 0;
+      left: 0;
+      z-index: 100;
+    }
+  }
+
+  $circleSize: 165px;
+  $radius: 100px;
+  $shadow: 0 0 10px 0 rgba(255,255,255,.35);
+  $fontColor: rgb(250,250,250);
+
+  @mixin object-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .profile-pic {
+    color: transparent;
+    transition: all .3s ease;
+    @include object-center;
+    position: relative;
+    transition: all .3s ease;
+
+    input {
+      display: none;
+    }
+
+    img {
+      position: absolute;
+      object-fit: cover;
+      width: $circleSize;
+      height: $circleSize;
+      box-shadow: $shadow;
+      border-radius: $radius;
+      z-index: 0;
+    }
+
+    .-label {
+      cursor: pointer;
+      height: $circleSize;
+      width: $circleSize;
+    }
+
+    &:hover {
+      .-label {
+        @include object-center;
+        background-color: rgba(0,0,0,.8);
+        z-index: 10000;
+        color: $fontColor;
+        transition: background-color .2s ease-in-out;
+        border-radius: $radius;
+        margin-bottom: 0;
+      }
+    }
+
+    span {
+      display: inline-flex;
+      padding: .2em;
+      height: 2em;
+    }
+  }
+
+  .disabled {
+    pointer-events: none;
+    cursor: default;
   }
 </style>
