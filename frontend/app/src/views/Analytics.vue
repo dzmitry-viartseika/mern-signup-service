168297a4acd1fc86cc293c87d3914e2b3b8523c6
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-03-24 14:26:04 +0300

    added first steps with charts

--- frontend/app/src/views/Analytics.vue
+++ frontend/app/src/views/Analytics.vue
@@ -3,12 +3,16 @@
     <h1 class="app__title">
       Analytics
     </h1>
+    <div class="app-analytics-section">
+      <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Chart } from 'highcharts-vue';
 import '@/assets/icons/Eye';
 
 @Component({
@@ -23,8 +27,27 @@ import '@/assets/icons/Eye';
       ],
     };
   },
+  components: {
+    highcharts: Chart,
+  },
 })
-export default class Analytics extends Vue {}
+export default class Analytics extends Vue {
+  get chartOptions() {
+    return {
+      title: {
+        text: 'Solar Employment Growth by Sector, 2010-2016'
+      },
+      subtitle: {
+        text: 'Source: thesolarfoundation.com'
+      },
+      series: [
+        {
+          data: [1, 2, 3],
+        },
+      ],
+    };
+  }
+}
 </script>
 
 <style scoped lang="scss">
