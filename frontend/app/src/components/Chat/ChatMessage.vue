47596a0593140ef92c1ce6588722fb7b2230f7e8
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-04-13 13:20:48 +0300

    added chat components

--- frontend/app/src/components/Chat/ChatMessage.vue
+++ frontend/app/src/components/Chat/ChatMessage.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="chat-message">
-    Chat Message
+  <div class="message-item__value">
+    <div
+      class="message-item__label"
+    >
+      LabelText
+    </div>
+    <span>
+      {{ item.message }}
+    </span>
   </div>
 </template>
 
 <script lang="ts">
-import Component from 'vue-class-component';
-import Vue from 'vue';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class ChatMessage extends Vue {
+  @Prop({ required: false, type: Object })
+  item: object;
 }
 </script>
 
