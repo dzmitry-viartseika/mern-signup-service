906a4abbf2701a705fce217fc546e718ffa4f660
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-04-13 13:16:52 +0300

    added chat components

---
+++ frontend/app/src/components/Chat/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-chat">
+    Chat
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+@Component({})
+export default class Chat extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<style scoped>
+
+</style>
---
+++ frontend/app/src/components/Chat/ChatMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chat-message">
+    Chat Message
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+@Component({})
+export default class ChatMessage extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
