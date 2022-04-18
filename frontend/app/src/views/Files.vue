73e0d29bcc94c9ed8bcf046ed40fadf22a72ae3e
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-04-18 17:37:29 +0300

    added icons for asideMen

--- frontend/app/src/views/Files.vue
+++ frontend/app/src/views/Files.vue
@@ -1,8 +1,23 @@
 <template>
-  <div class="app-notification">
-    <h1 class="app__title">
-      Files
-    </h1>
+  <div class="app-files">
+    <div class="app-files-header">
+      <h1 class="app__title">
+        Files
+      </h1>
+      <button
+        class="app__btn app__btn--primary"
+      >
+        Add file
+      </button>
+      <button
+        class="app__btn app__btn--primary"
+      >
+        Create Folder
+      </button>
+    </div>
+    <div class="app-files-content">
+
+    </div>
   </div>
 </template>
 
@@ -28,5 +43,21 @@ export default class Files extends Vue {}
 </script>
 
 <style scoped lang="scss">
+  .app-files {
 
+    &-header {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &-content {
+      margin-top: 20px;
+      box-shadow: 0 0 15px rgba(black, .2);
+      border-radius: 4px;
+      padding: 35px 24px 24px;
+      height: calc(100vh - 90px);
+      min-height: 309px;
+      overflow: auto;
+    }
+  }
 </style>
