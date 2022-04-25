e053f50ba223407be05ef5fcdca5f5bdce82ee50
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-04-25 11:09:54 +0300

    added jsDoc for WishesService

--- frontend/app/src/services/Wishes/Wishes.ts
+++ frontend/app/src/services/Wishes/Wishes.ts
@@ -2,8 +2,15 @@ import { AxiosResponse } from 'axios';
 import $api from '@/api/api';
 import { IAuthResponse } from '@/model/response/IAuthResponse';
 
+/**
+ * Класс для работы с пожеланиями пользователей системы
+ */
 export default class WishesService {
-
+  /**
+   * Отправка пожелания на почту Admin of системы
+   * @param email - почта текущего пользователя который отправляет сообщением
+   * @param text - сообщение для отправки
+   */
   static async sendWishesTextToEmail(email: string, text: string): Promise<AxiosResponse<IAuthResponse>> {
     return $api.post<IAuthResponse>('/send-wishes', { email, text });
   }
