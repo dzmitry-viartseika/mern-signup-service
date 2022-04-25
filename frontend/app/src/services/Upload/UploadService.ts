0b5a607af6bf88c065db5e59766aa2db6599c4e8
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-04-25 11:07:20 +0300

    added jsDoc for UploadService

--- frontend/app/src/services/Upload/UploadService.ts
+++ frontend/app/src/services/Upload/UploadService.ts
@@ -2,15 +2,33 @@ import { AxiosResponse } from 'axios';
 import $api from '@/api/api';
 import { IAuthResponse } from '@/model/response/IAuthResponse';
 
+/**
+ * Класс для взаимодействия с файлами
+ */
 export default class UploadService {
+  /**
+   * Загрузка файла на сервер
+   * @param file - загружаемый файл
+   * @return Promise - возращает промис с информацией о загруженном файле
+   */
   static async upload(file: any): Promise<AxiosResponse<IAuthResponse>> {
     return $api.post<IAuthResponse>('/singleFile', file);
   }
 
+  /**
+   * Получение файла по айдишке
+   * @param id - айдишка файла
+   * @return Promise - возращает промис с текущим файлом
+   */
   static async getFileById(id: string): Promise<AxiosResponse<IAuthResponse>> {
     return $api.get<IAuthResponse>(`/get-file/${id}`);
   }
 
+  /**
+   * Удаление файла по айдишке
+   * @param id - айдишка файла
+   * @return Promise - возращает промис с информацией о проведенной операции
+   */
   static async deleteFile(id: string): Promise<AxiosResponse<IAuthResponse>> {
     return $api.delete<any>(`/delete-file/${id}`);
   }
