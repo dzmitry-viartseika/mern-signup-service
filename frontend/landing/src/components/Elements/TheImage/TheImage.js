cc855b4683a65dc8896d5d78ce75ecf2f56e1054
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-04-27 15:18:14 +0300

    added default footer

--- frontend/landing/src/components/Elements/TheImage/TheImage.js
+++ frontend/landing/src/components/Elements/TheImage/TheImage.js
@@ -2,11 +2,7 @@ import React from "react";
 
 const Image = ({ name }) => {
     try {
-        console.log('name', name);
-        // Import image on demand
         const image = `../../assets/images/${name}`;
-        console.log('image', image);
-        // If the image doesn't exist. return null
         if (!image) return null;
         return <img src={image.default} />;
     } catch (error) {
